a0fca09cbe88309797dc2f87b3e5f5f466c95c71
Bartek <bartosz.skorka93@gmail.com>
2019-10-09 22:37:37 +0200

    init commit - udemy react course - 43 / 546

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: [
+            {name: 'Bartek', age: 26},
+            {name: 'Milena', age: 25},
+            {name: 'Anna', age: 30}
+        ],
+        othrState: 'some other value'
+    };
+
+    switchNameHandler = () => {
+        // console.log('Was clicked!')
+        this.setState({
+            persons: [
+                {name: 'Bartosz', age: 26},
+                {name: 'Milena', age: 25},
+                {name: 'Anna', age: 30}
+            ]
+        })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Henlo, I am a Barteks' React App! </h1>
+                <p>Super working sztuciec bejbe!</p>
+                <button onClick={this.switchNameHandler}>Switch name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I love my boyfriend very
+                    much!</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+    }
+
+//     return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Henlo, I am a Barteks\' React App!<3!!'));
 }
 
 export default App;
